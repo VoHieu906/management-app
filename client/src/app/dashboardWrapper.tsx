2fffbda329ceb6c848dd35506655de5be7065350
VoHieu906 <vohieutd09@gmail.com>
2025-01-12 09:27:34 +0700

    add sidebar

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Navbar from "./(components)/Navbar";
+import Sidebar from "./(components)/Sidebar";
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50">
+      <Sidebar />
       <main
         className={`dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64`}
       >
