d33a60c4de095201f6643129181a6e3b7ac35071
VoHieu906 <vohieutd09@gmail.com>
2025-01-13 21:39:19 +0700

    backend: project + task

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -9,6 +9,8 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const helmet_1 = __importDefault(require("helmet"));
 const morgan_1 = __importDefault(require("morgan"));
+const projectRoutes_1 = __importDefault(require("./routes/projectRoutes"));
+const taskRoutes_1 = __importDefault(require("./routes/taskRoutes"));
 // ROUTE IMPORT
 // CONFIGURATIONS
 dotenv_1.default.config();
@@ -24,6 +26,8 @@ app.use((0, cors_1.default)());
 app.get("/", (req, res) => {
     res.send("This is home route");
 });
+app.use("/projects", projectRoutes_1.default);
+app.use("/tasks", taskRoutes_1.default);
 //SERVE
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,8 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+import projectRoutes from "./routes/projectRoutes";
+import taskRoutes from "./routes/taskRoutes";
 // ROUTE IMPORT
 
 // CONFIGURATIONS
@@ -21,6 +23,8 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("This is home route");
 });
+app.use("/projects", projectRoutes);
+app.use("/tasks", taskRoutes);
 
 //SERVE
 const port = process.env.PORT || 3000;
