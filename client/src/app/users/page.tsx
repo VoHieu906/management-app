89e60def74c4c71a38561fd5e63a557ce58939a5
VoHieu906 <vohieutd09@gmail.com>
2025-01-16 21:36:53 +0700

    redirect images to s3

--- client/src/app/users/page.tsx
+++ client/src/app/users/page.tsx
@@ -31,7 +31,7 @@ const columns: GridColDef[] = [
       <div className="flex size-full items-center justify-center">
         <div className="size-9">
           <Image
-            src={`/${params.value}`}
+            src={`https://ma-s3-images.s3.us-east-1.amazonaws.com/${params.value}`}
             alt={params.row.username}
             width={100}
             height={50}
