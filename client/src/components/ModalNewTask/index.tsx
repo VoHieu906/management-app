84d4f1b8cc96649756a993e53111d597bd6c9149
VoHieu906 <vohieutd09@gmail.com>
2025-01-17 12:33:24 +0700

    fix form valid

--- client/src/components/ModalNewTask/index.tsx
+++ client/src/components/ModalNewTask/index.tsx
@@ -43,10 +43,10 @@ const ModalNewTask = ({ isOpen, onClose, id = null }: Props) => {
     });
   };
   const isFormValid = () => {
-    return title && authorUserId && !(id !== null || projectId);
+    return title && authorUserId && (id !== null || projectId);
   };
   const selectStyles =
-    "mb-4 bloc w-full rounded border border-gray-300 px-3 py-2 dark:border-dark-tertiary dark:bg-dark-tertiary dark:text-white dark:focus:outline-none";
+    "mb-4 block w-full rounded border border-gray-300 px-3 py-2 dark:border-dark-tertiary dark:bg-dark-tertiary dark:text-white dark:focus:outline-none";
   const inputStyles =
     "w-full rounded border border-gray-300 p-2 shadow-sm dark:border-dark-tertiary dark:bg-dark-tertiary dark:text-white dark:focus:outline-none";
   return (
